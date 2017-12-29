09755954946b1a0b0a38897f97f57d6bc637968a
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-12-28 20:55:22 -0600

    validate input, move list reorg to own function

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
 #!/usr/bin/env python3
 print("Give me a root note and I'll give you the major chord.")
 
+NOTES = [
+        'A',
+        'B♭',
+        'B',
+        'C',
+        'C♯',
+        'D',
+        'E♭',
+        'E',
+        'F',
+        'F♯',
+        'G',
+        'G♯',
+        ]
+
+def validate_note(note):
+    return note.upper() in NOTES
 
 def ask_for_note():
-    # Validate it.
-    return "B"
+    while True:
+        note = input("root note: ").upper()
+        is_valid = validate_note(note)
+        if (is_valid):
+            return note
+            break
+        else:
+            print("I don't understand that note.")
 
-# Ask for a root note.
-note = ask_for_note()
+
+def shift_list(the_list, item):
+    """Reorganize list so item is at 0 index."""
+    index = the_list.index(item)
+    before = the_list[:index]
+    the_list = the_list[index:]
+    the_list.extend(before)
+    return the_list
 
 def get_major_chord(root_note):
-    notes = [
-            'A',
-            'B♭',
-            'B',
-            'C',
-            'C♯',
-            'D',
-            'E♭',
-            'E',
-            'F',
-            'F♯',
-            'G',
-            'G♯',
-            ]
-
-    index = notes.index(root_note)
-
-    # reorganize the list so the root is at the beginning
-    before = notes[:index]
-    notes = notes[index:]
-    notes.extend(before)
-
-    third = notes[4]
-    fifth = notes[7]
+    shifted_notes = shift_list(NOTES, root_note)
+    third = shifted_notes[4]
+    fifth = shifted_notes[7]
     chord = '{:s}-{:s}-{:s}'.format(root_note, third, fifth)
     return chord
 
+note = ask_for_note()
 chord = get_major_chord(note)
-
-# Give the major chord.
 print('The major chord for the root {:s} is {:s}.'.format(note, chord))
